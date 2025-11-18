1d28a7cd68720394add9a4ca92cda5f369fb11a0
Sufyaan Sayed <sufyaansayed78@gmail.com>
2025-11-18 11:55:59 +0000

    libraries downloaded

---
+++ template.py
@@ -0,0 +1,36 @@
+import logging 
+import os 
+from pathlib import Path
+logging.basicConfig(level=logging.INFO , format="[%(asctime)s]:%(message)s:")
+
+list_of_files=[
+    f"src/__init__.py",
+    f"src/components/__init__.py",
+    f"src/utils/__init__.py",
+    f"src/utils/common.py",
+    f"src/configs/__init__.py",
+    f"src/configs/configuration.py",
+    f"src/pipeline/__init__.py",
+    f"src/entity/__init__.py",
+    f"src/entity/config_entity.py",
+    f"src/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "Dockerfile",
+    "setup.py",
+    "research/research.ipynb",
+    "template/index.html"
+]
+for path in list_of_files:
+    file_path = Path(path)
+    file_dir, file_name = os.path.split(file_path)
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Creating directory {file_dir} for the file : {file_name}")
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path,'wb') as f :
+            pass 
+
+
