8ce718ff9278c1dd0d7235a93c926ddf42bc0b24
Sufyaan Sayed <sufyaansayed78@gmail.com>
2025-11-18 23:31:32 +0000

    chunking function implemented

--- src/components/Data_Ingestion.py
+++ src/components/Data_Ingestion.py
@@ -5,6 +5,7 @@ from src.entity.config_entity import DataIngestionConfig
 from src.utils.common import read_yaml , create_directories,save_text
 import os
 from typing import IO
+import Pandas as pd 
 
 class DataIngestion:
     def __init(self,config : DataIngestionConfig):
@@ -26,12 +27,24 @@ def extract_text_from_pdf( self,pdf : IO[bytes]) -> str :
 
         save_text(self.config.text_path, text)
 
-def preprocess_text(self):
-    with open(self.config.text_path, 'r') as file:
-         text = file.read()
-    stopwords = nltk.corpus.stopwords.words('english')
-    text_as_list = text.splitlines()
-    
+def chunk_text(self, text: str, chunk_size: int = 500, overlap: int = 50) -> list:
+        from nltk.tokenize import word_tokenize
+
+        words = word_tokenize(text)
+        chunks = []
+        start = 0
+        text_length = len(words)
+
+        while start < text_length:
+            end = min(start + chunk_size, text_length)
+            chunk = ' '.join(words[start:end])
+            chunks.append(chunk)
+            start += chunk_size - overlap
+        df = pd.DataFrame(chunks, columns=['chunks'])
+        df.to_csv(self.config.token_path, index=False)
+
+        
+
 
     
 
